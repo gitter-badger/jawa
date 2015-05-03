fb9e7abf3a9d2a0214b96eecc3daa7a839472ac2
sankar <sroy@ksu.edu>
2015-05-02 19:05:10 -0500

    minor edit

--- sireum-jawa/src/main/scala/org/sireum/jawa/JawaResolver.scala
+++ sireum-jawa/src/main/scala/org/sireum/jawa/JawaResolver.scala
@@ -210,7 +210,7 @@ object JawaResolver {
    */
 	
 	def resolveFromST(st : SymbolTable, level : Center.ResolveLevel.Value, par : Boolean) : Unit = {
-    if(!JawaCodeSource.isPreLoaded) throw new RuntimeException("In whole program mode but library code did not been pre-loaded, call JawaCodeSource.preLoad first.")
+    if(!JawaCodeSource.isPreLoaded) throw new RuntimeException("In 'whole program' mode but library code has not been pre-loaded; call JawaCodeSource.preLoad first.")
     val stp = st.asInstanceOf[SymbolTableProducer]
 	  resolveRecords(stp, level, par)
 	  resolveGlobalVars(stp, level, par)
